23c003afe0b5903ca5320d52125f3a3c88e3e9c0
Max Nowak <56130196+Desoxyribose000@users.noreply.github.com>
2021-03-22 16:41:17 +0100

    Bug Fix

--- cli_data_purifier.py
+++ cli_data_purifier.py
@@ -24,13 +24,13 @@ if args['--help']:
     """Prints the usage as help."""
     print(__doc__)
 
-if args['filename']:
+if args['<filename>']:
     """Returns data from csv file."""
     try:
-        if args['sliced']:
-            data = data_purifier.extract_sliced_clean_data(args['filename'])
+        if args['--sliced']:
+            data = data_purifier.extract_sliced_clean_data(args['<filename>'])
         else:
-            data = data_purifier.extract_clean_data(args['filename'])
+            data = data_purifier.extract_clean_data(args['<filename>'])
     except Exception as e:
         print('Something went wrong: ' + str(e))
     else:
